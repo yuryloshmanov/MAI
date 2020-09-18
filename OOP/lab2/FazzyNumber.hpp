5f4c93911aec2edb74a18542a3f454c9afed2e25
Yury Loshmanov <yuryloshmanov@gmail.com>
2020-09-18 19:48:19 +0300

    Add lab2 empty files

---
+++ OOP/lab2/FazzyNumber.hpp
@@ -0,0 +1,19 @@
+//
+//  FazzyNumber.hpp
+//  lab2
+//  Группа М8О-206Б-19
+//  Вариант 4
+//  Created by Юрий Лошманов on 18.09.2020.
+//  Copyright © 2020 Юрий Лошманов. All rights reserved.
+//
+
+#ifndef FazzyNumber_hpp
+#define FazzyNumber_hpp
+
+#include <iostream>
+
+class FazzyNumber {
+    
+};
+
+#endif /* FazzyNumber_hpp */
---
+++ OOP/lab2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  lab2
+//  Группа М8О-206Б-19
+//  Вариант 4
+//  Created by Юрий Лошманов on 18.09.2020.
+//  Copyright © 2020 Юрий Лошманов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
