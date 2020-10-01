78bde56f583f1109baa116cd5095485e424a1cf1
yuryloshmanov <63449208+yuryloshmanov@users.noreply.github.com>
2020-10-01 10:46:32 +0300

    Update sort.hpp

--- DA/lab1/sort.hpp
+++ DA/lab1/sort.hpp
@@ -19,7 +19,7 @@ namespace sort {
         for (size_t i = 0; i < v.size(); i++) {
             table[v[i].first]++;
         }
-        for (size_t i = 1; i <= upperBound; i++) {
+        for (size_t i = 1; i < table.size(); i++) {
             table[i] = table[i] + table[i - 1];
         }
         for (size_t i = v.size() - 1;; i--) {
