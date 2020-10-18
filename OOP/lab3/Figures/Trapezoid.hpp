f42577e29e27f984da5b0826eb48e04d4512b186
yuryloshmanov <yuryloshmanov@gmail.com>
2020-10-18 12:29:34 +0300

    Complete trapezoid class

--- OOP/lab3/Figures/Trapezoid.hpp
+++ OOP/lab3/Figures/Trapezoid.hpp
@@ -8,8 +8,66 @@
 #ifndef Trapezoid_hpp
 #define Trapezoid_hpp
 
-class Trapezoid {
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+
+class Trapezoid: public Figure {
+    std::vector<std::pair<double, double>> coordinates;
     
+public:
+    Trapezoid(std::vector<std::pair<double, double>> coordinates);
+    
+    auto geometriсСenter() -> std::pair<double, double> override;
+    auto square() -> double override;
 };
+
+
+Trapezoid::Trapezoid(std::vector<std::pair<double, double>> coordinates) {
+    std::sort(coordinates.begin(), coordinates.end(), [](auto lhs, auto rhs) {
+        return lhs.first < rhs.first;
+    });
+    std::sort(coordinates.begin(), coordinates.end(), [](auto lhs, auto rhs) {
+        return lhs.second > rhs.second;
+    });
+
+    if (
+        !(coordinates[0].second == coordinates[1].second && coordinates[2].second == coordinates[3].second)
+    ) {
+        throw std::logic_error("Invalid trapezoid");
+    }
+    this->coordinates = coordinates;
+}
+
+
+auto Trapezoid::geometriсСenter() -> std::pair<double, double> {
+    auto [xa, ya] = coordinates[2];
+    auto [xb, yb] = coordinates[0];
+    auto [xc, yc] = coordinates[1];
+    auto [xd, yd] = coordinates[3];
+
+    auto A = xc - xa;
+    auto B = yc - ya;
+    auto C = xd - xb;
+    auto D = yd - yb;
+    
+    auto x = (yb + xa * B / A - xb * D / C - ya) / (B / A - D / C);
+    auto y = (xb + ya * A / B - yb * C / D - xa) / (A / B - C / D);
+    
+    return std::make_pair(x, y);
+}
+
+
+auto Trapezoid::square() -> double {
+    auto a = coordinates[1].first - coordinates[0].first;
+    auto b = coordinates[3].first - coordinates[2].first;
+    
+    auto h = coordinates[0].second - coordinates[2].second;
+    
+    return h * (a + b) / 2;
+}
+
 
 #endif /* Trapezoid_hpp */
