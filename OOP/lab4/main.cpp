5d7d57f288ea6a75b2a912305c4a5b6679ef4997
yuryloshmanov <yuryloshmanov@gmail.com>
2020-10-24 22:16:24 +0300

    Small changes

--- OOP/lab4/main.cpp
+++ OOP/lab4/main.cpp
@@ -19,45 +19,50 @@
 #include "Figures/Trapezoid.hpp"
 #include "Figures/Rhombus.hpp"
 
+auto randInt() -> int {
+    return rand() % 20 * pow(-1, 1 + rand() % 2);
+}
 
 template <class T>
 auto randomRectangle() -> Rectangle<T> {
-    auto x1 = rand() % 1000;
-    auto x2 = rand() % 1000;
+    auto x1 = randInt();
+    auto x2 = randInt();
     
-    auto y1 = rand() % 1000;
-    auto y2 = rand() % 1000;
+    auto y1 = randInt();
+    auto y2 = randInt();
     
     return Rectangle<T>({{x1, y1}, {x1, y2}, {x2, y1}, {x2, y2}});
 }
 
 template <class T>
 auto randomTrapezoid() -> Trapezoid<T> {
-    auto x1 = rand() % 1000;
-    auto x2 = rand() % 1000;
-    auto x3 = rand() % 1000;
-    auto x4 = rand() % 1000;
+    auto x1 = randInt();
+    auto x2 = randInt();
+    auto x3 = randInt();
+    auto x4 = randInt();
     
-    auto y1 = rand() % 1000;
-    auto y2 = rand() % 1000;
+    auto y1 = randInt();
+    auto y2 = randInt();
     
     return Trapezoid<T>({{x1, y1}, {x2, y2}, {x3, y1}, {x4, y2}});
 }
 
 template <class T>
 auto randomRhombus() -> Rhombus<T> {
-    auto x = rand() % 1000;
-    auto dx = rand() % 1000;
+    auto x = randInt();
+    auto dx = randInt();
     
-    auto y = rand() % 1000;
-    auto dy = rand() % 1000;
+    auto y = randInt();
+    auto dy = randInt();
     
     return Rhombus<T>({{x - dx, y}, {x, y + dy}, {x + dx, y}, {x, y - dy}});
 }
 
 auto main() -> int {
-    auto tuple = std::make_tuple(randomRectangle<double>(), randomTrapezoid<double>(), randomRhombus<double>());
-
+    auto tuple = std::make_tuple(
+        randomRectangle<double>(), randomTrapezoid<double>(), randomRhombus<double>(),
+        randomRectangle<double>(), randomTrapezoid<double>(), randomRhombus<double>()
+    );
     for (std::string line; std::getline(std::cin, line);) {
         std::stringstream ss(line);
         std::string command;
